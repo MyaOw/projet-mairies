0e39798e60416685eec0f9fd98b050444ac3fb71
Pauline <mya@mya-ow.net>
2018-07-19 17:38:58 +0200

    modifs townhalls et mairie scrappers.rb

--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -2,61 +2,40 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'json'
-
-class Mairie
-  attr_accessor :name, :departement, :email
-  def initialize(name, departement,email) 
-    @name = name
-    @departement = departement
-    @email = email
-  end
-
-  def as_json(options={})
-        {
-            name: @name,
-            departement: @departement,
-            email: @email
-        }
-        
-    end
-end
+require_relative "mairie_scrapper"
 
 class Scrapping
   include Enumerable
 
-  def mairie_page(town)
+  def mairie_page(town)   # Rapporte la page de la mairie
     page = Nokogiri::HTML(open(town)) rescue false
     return page.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]').text if page != false
     return "Page introuvable"
   end
 
-  def mairie_dept(town)
+  def mairie_dept(town)   # Rapporte le département en toutes lettres
     page = Nokogiri::HTML(open(town)) rescue false
     return page.xpath('/html/body/div/main/section[4]/div/table/tbody/tr[1]/td[2]').text if page != false
     return "Page introuvable"
   end
   
-  def departement_page(dept)
+  def departement_page(dept)  # Rapporte la page du département
     page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/#{dept}.html"))
       links = page.css("a.lientxt") 
 end
   
-  ############################################################
+  
   def perform
-    @name = []
-    @dep = []
-    @mail = []
-    array = ["rhone", "rhone-2", "loire", "drome", "drome-2"]
     mairies = []
+    array = ["rhone", "rhone-2", "loire", "drome", "drome-2"]
     array.each do |x|    
 
-    url_all = departement_page(x)
+    url_all = departement_page(x)  # Rapporte les pages de département depuis l'array
     url_all.each do |url| 
-      name = url.text
+      name = url.text   #
       dep = mairie_dept(url['href'].sub("./", "http://annuaire-des-mairies.com/"))
       mail = mairie_page(url['href'].sub("./", "http://annuaire-des-mairies.com/")) 
       mairies.push(Mairie.new(name, dep, mail))
-      
     end
     end
     
@@ -64,8 +43,6 @@ end
       |mairie| puts "#{mairie.name} #{mairie.departement} #{mairie.email}"
     }
     
-  
-    # puts @hash = @name.zip(@dep, @mail).to_h
     File.open("../../db/scrap.json","w") do |f|
       mairies.each{
         |mairie|
