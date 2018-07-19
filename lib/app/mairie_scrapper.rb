b31b951279f82b7bcbd79fe23dd79cbbfb9736fd
Pauline <mya@mya-ow.net>
2018-07-19 20:14:01 +0200

    comments mairie_scrapper.rb

--- lib/app/mairie_scrapper.rb
+++ lib/app/mairie_scrapper.rb
@@ -1,12 +1,12 @@
-class Mairie
-  attr_accessor :name, :departement, :email
+class Mairie   # La classe Mairie correspond à l'objet mairie
+  attr_accessor :name, :departement, :email  # L'objet a 3 variables
   def initialize(name, departement,email) 
     @name = name
     @departement = departement
     @email = email
   end
 
-  def as_json(options={})
+  def as_json(options={})  # permet de transformer notre array en json
       return {
             "name"  =>  @name,
             "departement"  =>  @departement,
