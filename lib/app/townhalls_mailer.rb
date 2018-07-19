fb115283b8933de0f214315170327a736a2106ed
Mario <mario.antoniali@gmail.com>
2018-07-19 20:38:34 +0200

    reajust mailer

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -9,12 +9,12 @@ class Mailer
     Gmail.connect(ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]) do |gmail|
     
       email = gmail.compose do
-        to "jihane.thp@gmail.com"
+        to "#{???}"
         subject "The Hacking Project!"
         body "Bonjour,\n
         Nous sommes les Yatsu, élèves à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.\n
         \n  
-        Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{mairie} #{send_to} veut changer le monde avec nous ?\n
+        Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{mairie} veut changer le monde avec nous ?\n
         \n  
         \n
         Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
